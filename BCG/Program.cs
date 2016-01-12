22bed98b4f7c5dc85b700e1f73e5446308a7eb67
denicoloyann <yanndenicolo@gmail.com>
2016-01-12 14:03:29 +0100

    Fenêtre accueil_Yann + Help
    Ajout d'une form 'Accueil_Yann' et 'Help'

--- BCG/Program.cs
+++ BCG/Program.cs
@@ -16,7 +16,7 @@ namespace BCG
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Accueil());
+            Application.Run(new Help());
         }
     }
 }
